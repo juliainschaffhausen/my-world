262d210944e57ba0d1faadf5eacad6bb40e9b0be
juliainschaffhausen <julglushkova@gmail.com>
2024-05-17 22:52:29 +0200

    refactor

--- my-world/src/app/app.component.ts
+++ my-world/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ThemeTogglerComponentComponent} from "./theme-toggler-component/theme-toggler-component.component";
 import {HeaderComponent} from "./header/header.component";
+import {CarouselComponent} from "./carousel/carousel.component";
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ThemeTogglerComponentComponent, HeaderComponent],
+  imports: [RouterOutlet, ThemeTogglerComponentComponent, HeaderComponent, CarouselComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
